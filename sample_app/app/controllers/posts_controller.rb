a3bfd5df210329f4868eddbc3ef4208c381b9d49
oosawagithub <newstation1215@gmail.com>
2019-05-08 12:14:59 +0900

    Initial commit message

---
+++ sample_app/app/controllers/posts_controller.rb
@@ -0,0 +1,61 @@
+class PostsController < ApplicationController
+
+  def new
+
+  	#空のモデルをビューへ渡す
+  	@post = Post.new
+
+  end
+
+  #以下を追加
+  def create
+
+  	#ストロングパラメーターを使用
+  	post = Post.new(post_params)
+
+  	#DBへ保存する
+  	post.save
+
+    #詳細画面へリダイレクト
+  	redirect_to post_path(post.id)
+
+  end
+
+  def index
+
+    @posts = Post.all
+
+  end
+
+  def show
+
+    @post = Post.find(params[:id])
+
+  end
+
+  def edit
+
+    @post = Post.find(params[:id])
+
+  end
+
+  def update
+
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+
+  end
+
+#privateは一種の境界線で、「ここから下はcontrollerの中でしか呼び出せません」という意味があります。
+#privateはControllerファイルの一番下のendのすぐ上に書いて下さい。
+
+  private
+
+  def post_params
+
+  	params.require(:post).permit(:title, :body)
+
+  end
+
+end
---
+++ sample_app/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'posts/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top' => 'root#top'
+
+  post 'posts' => 'posts#create'
+
+  get 'posts' => 'posts#index'
+
+  get 'posts/:id' => 'posts#show',as: 'post'
+  #投稿１件ごとの詳細を表示
+  #.../posts/1 や .../posts/3 に該当する
+
+  get 'posts/:id/edit' => 'posts#edit', as: 'edit_post'
+
+  patch 'posts/:id' => 'posts#update', as: 'update_post'
+end
